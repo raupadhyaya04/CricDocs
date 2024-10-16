2a5d86f4beb51039eb95ec730df0f73b51a70f56
Raj Aryan Upadhyaya <rajaryanupadhyaya@gmail.com>
2024-10-16 23:28:18 +0100

    MOST CERTAINLY FIXED FORM HANDLER NOW

--- sessionPlanGenerator.py
+++ sessionPlanGenerator.py
@@ -1,20 +1,23 @@
 def generate_session_plan_text(age, cgoals, pgoals, date, venue, duration, equipment, safety, warmup, skills, drills, games, cooldown, eval):
     title = f"""Session Plan:\n
 """
-    deets = f"""Date: {date}
-Coach Outcomes: {cgoals}
-Player Outcomes: {pgoals}
-Age Group: {age}
-Duration: {duration}
-Venue: {venue}
-Equipment: {equipment}
-Safety Considerations: {safety}"""
+    deets = f"""
+Date: {date}\n
+Coach Outcomes: {cgoals}\n
+Player Outcomes: {pgoals}\n
+Age Group: {age}\n
+Duration: {duration}\n
+Venue: {venue}\n
+Equipment: {equipment}\n
+Safety Considerations: {safety}\n"""
     
-    session = f"""Warm up: {warmup}
-Skills: {skills}
-Drills: {drills}
-Games: {games}"""
-    eOfSession = f"""Cooldown: {cooldown}
+    session = f"""
+Warm up: {warmup}\n
+Skills: {skills}\n
+Drills: {drills}\n
+Games: {games}\n"""
+    eOfSession = f"""
+Cooldown: {cooldown}\n
 Evaluation (Key points/learnings, etc): {eval}"""
     text = title + deets + session + eOfSession
     return text
