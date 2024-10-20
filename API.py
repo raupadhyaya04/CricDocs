07f023acc1787c2074ede2c7db0f2fcacc9857c9
Raj Aryan Upadhyaya <rajaryanupadhyaya@gmail.com>
2024-10-20 14:44:57 +0100

    Added some small imports

--- API.py
+++ API.py
@@ -5,6 +5,10 @@ from creating_pdf import create_pdf
 import os
 from configurer import configure
 
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+
 
 app = Flask(__name__)
 
